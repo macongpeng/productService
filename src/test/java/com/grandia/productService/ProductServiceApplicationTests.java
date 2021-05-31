171b0f9aed44c9f5a924c5d75b25c9835ce7e841
Denny Ma <dma@infomedia.com.au>
2021-05-31 10:58:11 +1000

    Set username for postgres

--- src/test/java/com/grandia/productService/ProductServiceApplicationTests.java
+++ src/test/java/com/grandia/productService/ProductServiceApplicationTests.java
@@ -3,7 +3,9 @@ package com.grandia.productService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 class ProductServiceApplicationTests {
 
 	@Test
